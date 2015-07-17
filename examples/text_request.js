f7ecf8dba9bc3cf0a814533218f4d9dcefeb54e6
Dmitriy Kuragin <kuragin@speaktoit.com>
2015-07-17 14:34:34 +0600

    pakage fixes

--- examples/text_request.js
+++ examples/text_request.js
@@ -4,7 +4,8 @@
  * MIT Licensed
  */
 
-var apiai = require("../module/apiai");
+// var apiai = require("../module/apiai");
+var apiai = require("apiai");
 
 var app = apiai("3485a96fb27744db83e78b8c4bc9e7b7", "cb9693af-85ce-4fbf-844a-5563722fc27f");
 
