6c7b9b3c8e335c2c791a0b0a8b13f37afed94dbd
Dmitriy Kuragin <kuragin@speaktoit.com>
2016-07-26 10:19:23 +0600

    fixe for voice request example

--- examples/voice_request.js
+++ examples/voice_request.js
@@ -9,9 +9,7 @@ var fs = require("fs");
 // var apiai = require("../module/apiai");
 var apiai = require("apiai");
 
-// var app = apiai("YOUR_ACCESS_TOKEN", "YOUR_SUBSCRIPTION_KEY");
-
-var app = apiai("YOUR_ACCESS_TOKEN", "YOUR_SUBSCRIPTION_KEY", {
+var app = apiai("YOUR_ACCESS_TOKEN", {
     language: 'ru'
 });
 
