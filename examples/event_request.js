a5d4673c85d5ad92def6dd9b6d0d9064f9442f07
Dmitriy Kuragin <kuragin@speaktoit.com>
2016-11-23 14:55:50 +0700

    missing file for event request. example for events.

---
+++ examples/event_request.js
@@ -0,0 +1,38 @@
+/*!
+ * apiai
+ * Copyright(c) 2015 http://api.ai/
+ * Apache 2.0 Licensed
+ */
+
+var util = require('util')
+var apiai = require("../module/apiai");
+// var apiai = require("apiai");
+
+var options = {
+    hostname: 'eap.api.ai',
+};
+
+var app = apiai("<YOUR_ACCESS_TOKEN>", options);
+
+var event = {
+    name: "network.connect",
+    data: {
+        param1: "param1 value",
+    }
+}
+
+var options = {
+    sessionId: '<UNIQE SESSION ID>'
+}
+
+var request = app.eventRequest(event, options);
+
+request.on('response', function(response) {
+    console.log(util.inspect(response, false, null))
+});
+
+request.on('error', function(error) {
+    console.log(error);
+});
+
+request.end();
